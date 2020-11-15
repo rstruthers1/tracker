53d620e2b54ea825bc12320f199b51e6fcfc0819
Rachel Struthers <rachel.struthers@sap.com>
2020-11-15 07:34:10 -0600

    MYF-20: Spike: try using react-table for list of food items

--- client/src/components/Foods.js
+++ client/src/components/Foods.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from "react";
-import FoodService from "../services/food.service";
-import {cellWidths} from "../utils/tracker.constants";
+import { useTable, usePagination, useFilters } from 'react-table';
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
+import FoodService from "../services/food.service";
 
 const Foods = (props) => {
 
@@ -18,28 +20,176 @@ const Foods = (props) => {
       });
   }, []);
   
+  const populateTableRows = (data) => {
+    let rows = [];
+    if (!data) {
+      return rows;
+    }
+    for (let i = 0; i < data.length; i++) {
+      let dataItem = data[i];
+      rows.push({
+        description: dataItem.description,
+        servingSize: dataItem.servingSize,
+        calories: dataItem.calories
+      });
+    };
+    return rows;
+  };
+
+  
+
+    const columns = React.useMemo(
+    () => [
+          {
+            Header: 'Description',
+            accessor: 'description',
+            sortType: 'basic'
+          },
+          {
+            Header: 'Serving Size',
+            accessor: 'servingSize',
+            disableFilters: true
+          },
+          {
+            Header: 'Calories',
+            accessor: 'calories',
+            disableFilters: true
+          },
+     
+    ],
+    []
+  );
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: TextFilter,
+    }),
+    []
+  )
+
+
+
+  const data = React.useMemo(() =>
+      populateTableRows(foods),
+    [foods]
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = useTable({ columns, data, defaultColumn, initialState: { pageIndex: 0 }, }, useFilters, usePagination)
+
+
+  function TextFilter({
+                        column: { filterValue, preFilteredRows, setFilter },
+                      }) {
+    const count = preFilteredRows.length;
+
+    return (
+      <input
+        value={filterValue || ''}
+        onChange={e => {
+          setFilter(e.target.value || undefined)
+        }}
+        placeholder={`Search ${count} records...`}
+      />
+    )
+  }
   
   return (
-    <div className="container">
+    <div className="container" style={{paddingBottom: "50px"}}>
       <h1>Foods</h1>
       {
-        foods ? (
-            <table>
+        data ? (
+          <div>
+            <Table {...getTableProps()}>
               <thead>
-              <tr><th>Description</th><th>Serving Size</th><th>Calories</th></tr>
+              {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render('Header')}
+                      <span style={{paddingLeft: "10px"}}>{column.canFilter ? column.render('Filter') : null}</span>
+                    </th>
+                  ))}
+                 
+                </tr>
+              ))}
               </thead>
-              <tbody>
-              {
-                foods.map((row) => (
-                  <tr key={row.id}>
-                    <td style={{width: cellWidths.DESCRIPTION}}>{row.description}</td>
-                    <td style={{width: cellWidths.SERVING_SIZE}}>{row.servingSize}</td>
-                    <td e={{width: cellWidths.CALORIES}}>{row.calories}</td>
+              <tbody {...getTableBodyProps()}>
+              {page.map((row, i) => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    })}
                   </tr>
-                ))
-              }
+                )
+              })}
               </tbody>
-            </table>
+            </Table>
+            <span style={{paddingRight: "10px"}}>
+              <Button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                {'<<'}
+              </Button>{' '}
+              <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {'<'}
+              </Button>{' '}
+              <Button onClick={() => nextPage()} disabled={!canNextPage}>
+                {'>'}
+              </Button>{' '}
+              <Button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                {'>>'}
+              </Button>{' '}
+            </span>
+              <span style={{paddingRight: "10px"}}>
+          Page{' '}
+                <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+              <span style={{paddingRight: "20px"}}>
+          Go to page:{' '}
+                <input
+                  type="number"
+                  defaultValue={pageIndex + 1}
+                  onChange={e => {
+                    const page = e.target.value ? Number(e.target.value) - 1 : 0
+                    gotoPage(page)
+                  }}
+                  style={{ width: '100px' }}
+                />
+        </span>
+              <select
+                value={pageSize}
+                onChange={e => {
+                  setPageSize(Number(e.target.value))
+                }}
+              >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+            
+          </div>
           ) :
           (
             <div>No foods</div>
